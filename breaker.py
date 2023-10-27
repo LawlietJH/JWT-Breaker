05ea381c4c8425c66d3f5db8a1cf4cbec91ca0f4
Lawliet <jerry_howl_lawliet@hotmail.com>
2023-10-27 01:07:46 -0600

    Agregado recibir el JWT como argumento del script

--- breaker.py
+++ breaker.py
@@ -1,40 +1,78 @@
 import base64
 import json
-import time
+import sys
+
 import jwt
 
+from modules import ArgParser
 
-encoded_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIifQ.p5LogGsW6l9h-xwx_QD8u61gpRTJnnQQvLl1dOVGQWQ"
 
-padding_b64 = lambda b64str: b64str + '=' * (-len(b64str) % 4)
-decode_b64 = lambda b64str: json.loads(base64.b64decode(padding_b64(b64str)).decode())
+def decode_b64(b64str) -> str:
+    b64str = b64str + '='*(-len(b64str) % 4)
+    b64str = base64.b64decode(b64str).decode()
+    return json.loads(b64str)
 
-header_b64, payload_b64, signature_b64 = encoded_jwt.split('.')
-header = decode_b64(header_b64)
-payload = decode_b64(payload_b64)
-print('\n Header:   ', header_b64)
-print('  - Decode:', header)
-print(' Payload:  ', payload_b64)
-print('  - Decode:', payload)
-print(' Signature:', signature_b64)
-algorithm = header.get('alg', '')
 
-filename = 'common_secrets.txt'
+def breaker(encoded_jwt: str, verbose: bool = False) -> str:
+    # encoded_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIifQ.p5LogGsW6l9h-xwx_QD8u61gpRTJnnQQvLl1dOVGQWQ"
 
-with open(filename, 'r') as file:
-    secrets = file.readlines()
+    header_b64, payload_b64, signature_b64 = encoded_jwt.split('.')
+    header = decode_b64(header_b64)
+    if verbose:
+        payload = decode_b64(payload_b64)
+        print('\n Header:   ', header_b64)
+        print('  - Decode:', header)
+        print(' Payload:  ', payload_b64)
+        print('  - Decode:', payload)
+        print(' Signature:', signature_b64)
+    # RFC 7518 - Alg List: https://tools.ietf.org/html/rfc7518#section-3
+    algorithm = header.get('alg', '')
 
-last_secret = ''
-print()
+    filename = 'common_secrets.txt'
 
-for secret in secrets:
-    time.sleep(.1)
-    if secret.endswith('\n'):
-        secret = secret[:-1]
-    try:
-        decoded_jwt = jwt.decode(encoded_jwt, secret, algorithms=['RS256', 'HS256', algorithm])
-        print('\r Secret:', secret, end=' '*len(last_secret)+'\n')
-        break
-    except Exception as error:
-        print('\r Secret:', secret, end=' '*len(last_secret))
-    last_secret = secret
+    with open(filename, 'r') as file:
+        secrets = file.readlines()
+
+    if verbose:
+        print()
+
+    last_secret = ''
+
+    for secret in secrets:
+        if secret.endswith('\n'):
+            secret = secret[:-1]
+        try:
+            jwt.decode(encoded_jwt, secret,
+                       algorithms=['RS256', 'HS256', algorithm])
+            if verbose:
+                print('\r Secret:', secret, end=' '*len(last_secret)+'\n')
+            return secret
+        except Exception as error:
+            if verbose:
+                print('\r Secret:', secret, end=' '*len(last_secret))
+        last_secret = secret
+
+    return ''
+
+
+def arguments_parser() -> str:
+    arg_parser = ArgParser()
+    rules = {'pairs': {'JWT': ['-jwt', '--jwt', '-t', '--token']}}
+    arguments, _ = arg_parser.parser(rules, sys.argv, wn=True)
+    return arguments
+
+
+def get_jwt(arguments: dict):
+    JWT = arguments.get('JWT')
+    assert JWT, "\n El argumento JWT Es Necesario."
+    return JWT[1]
+
+
+if __name__ == '__main__':
+    arguments = arguments_parser()
+    JWT = get_jwt(arguments)
+    secret = breaker(JWT)
+    if not secret:
+        print("\n [-] Secret Not Found!")
+    else:
+        print(f"\n [+] Secret: {repr(secret)}")
