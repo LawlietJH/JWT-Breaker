e6f6b6d2b5e863c6297feb48841a6d075e2a31e5
Lawliet <jerry_howl_lawliet@hotmail.com>
2023-10-27 21:03:36 -0600

    Actualizado módulo: ArgParser v1.0.4

--- modules/argparser.py
+++ modules/argparser.py
@@ -1,83 +1,83 @@
 # Tested in: Python 3.11.0
 # By: LawlietJH
-# ArgParser v1.0.1
+# ArgParser v1.0.3
 # Descripción: Módulo para análisis y extracción de argumentos.
 #              Permite mediante reglas obtener valores de los argumentos.
 #              Permite analizar una cadena o una lista/tupla de
 #              argumentos (como 'sys.argv' así directamente).
+# TODO: Implementar funcionalidad de Auto Generación de help. Extensión de rules.
+# TODO: Implementar selección de Argumentos Obligatorios. Extensión de rules.
 
 from typing import Optional
 
 # =======================================================================
 __author__ = 'LawlietJH'   # Desarrollador
 __title__ = 'ArgParser'    # Nombre
-__version__ = 'v1.0.1'     # Version
+__version__ = 'v1.0.4'     # Version
 # =======================================================================
 
 
 class ArgParser:
 
     class MissingArgument(Exception):
-        def __init__(self, error_msg=''): self.error_msg = error_msg
+        def __init__(self, error_msg: str = ''): self.error_msg = error_msg
         def __str__(self): return repr(self.error_msg)
 
-    def __init__(self, rules: Optional[dict] = None, args: Optional[str | list] = None,
-                 wasv: Optional[bool] = False, wn: Optional[bool] = False):
-        """
-        rules: Rules for parsing arguments.
-        args:  Arguments to parse.
-        wasv:  Output with all single values (true and false).
-        wn:    Output with names. example: {'Name': ('argument', 'value')}.
-        """
+    def __init__(self, rules: Optional[dict] = None, args: Optional[str | list] = None) -> None:
+        """ rules: Rules for parsing arguments.
+            args:  Arguments to parse. """
         self.rules = rules
         self.args = args
-        self.wasv = wasv
-        self.wn = wn
-        self.help = '''Example...\n
+        self.keys = False
+        self.wasv = False
+        self.ignored = False
+        self.help = '''\n
 		\r rules = {
-		\r     'pairs':  {  # 'arg value'               # Use:
-		\r         'Name 1': ['-e', '--example'],       # -e value, --example value
-		\r         'Name 2': '-o',                      # -o value
-		\r         'Wordlist': '-w'                     # -w value
+		\r     'pairs':  { # Key Value                  # Use examples:
+		\r         'Key 1': ['-i', '--input'],          # -i value, --input value
+		\r         'Key 2': '-r',                       # -r value, -r = value, -r: value
+		\r         'Key 3': ('-o', '--output'),         # -o=value, --output:value
+		\r         'Key 4': '-dn',                      # -dn= value, -dn value, -dn =value
+		\r         'Key 5': ['-xn', '-xname', 'xn'],    # -xn: value, -xname = value, xn : value
+		\r         'Key 6': '-w'                        # -w value, -w=value, -w:value, -w :value
 		\r     },
-		\r     'single': {  # 'arg'
-		\r         'Name 3': ['-n', '--name'],          # -n, --name
-		\r         'Name 4': '-a',                      # -a
-		\r         'Name 5': 'val'                      # val
-		\r     },
-		\r     'united': {  # 'arg = value' or 'arg: value'
-		\r         'Name 6': ('-vn', '--valuename'),    # -vn=value, --valname:value
-		\r         'Name 7': '-dn',                     # -dn= value
-		\r         'Name 8': ['-xn', '-xname', 'xn'],   # -xn: value, -xname = value, xn : value
-		\r         'Wordlist': '-w'                     # -w: value
-		\r                                              # (-w alternative value to -w in 'pairs' rules)
+		\r     'single': { # Bool
+		\r         'Key 7': ['-n', '--name'],           # -n, --name
+		\r         'Key 8': '-a',                       # -a
+		\r         'Key 9': 'val'                       # val
 		\r     }
 		\r }
 		'''
 
-    def _set_args(self, rules, args, wasv, wn):
-
-        if not self.rules and not rules:
-            raise self.MissingArgument(
-                "El argumento 'rules' es obligatorio.")
+    def _set_args(self, args: tuple | list | str, rules: dict,
+                  wasv: bool, keys: bool, ignored: bool) -> None:
 
         if not self.args and not args:
             raise self.MissingArgument(
-                "El argumento 'args' es obligatorio.")
+                "El argumento 'args' es necesario.")
 
-        assert isinstance(rules, dict),  self.help
-        assert isinstance(args, (tuple, list, str))
+        if not self.rules and not rules:
+            raise self.MissingArgument(
+                "El argumento 'rules' es necesario.")
 
-        self.rules = rules
-        self.args = args
+        if args:
+            assert isinstance(args, (tuple, list, str))
+            self.args = args
 
-        if not self.wasv and wasv:
-            self.wasv = True
+        if rules:
+            assert isinstance(rules, dict),  self.help
+            self.rules = rules
 
-        if not self.wn and wn:
-            self.wn = wn
+        if isinstance(wasv, bool):
+            self.wasv = wasv
 
-    def pairs_union(self, args):
+        if isinstance(keys, bool):
+            self.keys = keys
+
+        if isinstance(ignored, bool):
+            self.ignored = ignored
+
+    def pairs_union(self, args: list) -> list:
         # Union of params with '=' or ':'
         tmp = []
         concat = ''
@@ -171,16 +171,28 @@ class ArgParser:
 
         return args
 
-    def pairs_vals(self, arg, args, pairs, output, wn):
+    def pairs_vals(self, arg: str, args: list, pairs: dict, output: dict) -> bool:
+
+        # Pairs united:
+        if ':' in arg or '=' in arg:
+            tmp_arg = arg.split(':')
+            if len(tmp_arg) != 2:
+                tmp_arg = tmp_arg[0].split('=')
+                if len(tmp_arg) != 2:
+                    return True
+            arg = tmp_arg[0]
+            args.insert(0, tmp_arg[1])
+
+        if not args:
+            return True
+
         for key, val in pairs.items():
-            if isinstance(val, (list, tuple)) and not arg in val:
-                continue
-            elif isinstance(val, str) and not arg == val:
+            if not arg in val and not arg == val:
                 continue
             if isinstance(val, (list, tuple, str)):
-                if wn and not key in output:
+                if self.keys and not key in output:
                     output[key] = (arg, args.pop(0))
-                elif (wn and key in output) or key in self.keys_used:
+                elif (self.keys and key in output) or key in self.keys_used:
                     return True
                 elif not arg in output:
                     output[arg] = args.pop(0)
@@ -188,63 +200,22 @@ class ArgParser:
                 return False
         return True
 
-    def single_vals(self, arg, single, output, wn):
-        ignore = True
-
+    def single_vals(self, arg: str, single: dict, output: dict) -> bool:
         for key, val in single.items():
+            if not arg in val and not arg == val:
+                continue
+            if isinstance(val, (list, tuple, str)):
+                if self.keys and not key in output:
+                    output[key] = (arg, True)
+                elif (self.keys and key in output) or key in self.keys_used:
+                    return True
+                elif not arg in output:
+                    output[arg] = True
+                    self.keys_used.append(key)
+                return False
+        return True
 
-            if val.__class__ in [list, tuple]:
-                if arg in val:
-                    if wn:
-                        output[key] = (arg, True)
-                    else:
-                        output[arg] = True
-                    ignore = False
-                    break
-            elif val.__class__ == str:
-                if arg == val:
-                    if wn:
-                        output[key] = (arg, True)
-                    else:
-                        output[arg] = True
-                    ignore = False
-                    break
-
-        return ignore
-
-    def united_vals(self, arg, united, output, ignored, wn):
-        tmp_arg = arg.split(':')
-
-        if len(tmp_arg) != 2:
-            tmp_arg = tmp_arg[0].split('=')
-            if len(tmp_arg) != 2:
-                ignored.append(arg)
-                return 'continue'
-
-        ignore = True
-
-        for key, val in united.items():
-
-            if val.__class__ in [list, tuple]:
-                if tmp_arg[0] in val:
-                    if wn and not key in output:
-                        output[key] = (tmp_arg[0], tmp_arg[1])
-                    elif not tmp_arg[0] in output:
-                        output[tmp_arg[0]] = tmp_arg[1]
-                    ignore = False
-                    break
-            elif val.__class__ == str:
-                if tmp_arg[0] == val:
-                    if wn and not key in output:
-                        output[key] = (tmp_arg[0], tmp_arg[1])
-                    elif not tmp_arg[0] in output:
-                        output[tmp_arg[0]] = tmp_arg[1]
-                    ignore = False
-                    break
-
-        return ignore
-
-    def strings_parser(self, args):
+    def strings_parser(self, args: list) -> list[str]:
         tmp = []
         init = False
         char = ''
@@ -297,25 +268,58 @@ class ArgParser:
                 tmp.append(arg)
         return tmp
 
-    def parser(self, rules: Optional[dict] = None, args: Optional[str | list] = None,
-               wasv: Optional[bool] = False, wn: Optional[bool] = False):
+    def _set_wasv(self, single: dict, output: dict) -> None:
+        if self.keys:
+            arguments = [argument for argument, _ in output.values()]
+            for key, value in single.items():
+                if isinstance(value, (list, tuple)):
+                    in_output = [False, value[0]]
+                    for val in value:
+                        if val in arguments:
+                            in_output = [True, val]
+                            break
+                    if not in_output[0]:
+                        output[key] = (in_output[1], False)
+                elif not key in arguments:
+                    output[key] = (value, False)
+            return
 
-        self._set_args(rules, args, wasv, wn)
+        for value in single.values():
+            if isinstance(value, (list, tuple)):
+                in_output = [False, value[0]]
+                for val in value:
+                    if val in output:
+                        in_output = [True, val]
+                        break
+                if not in_output[0]:
+                    output[in_output[1]] = False
+            elif not value in output:
+                output[value] = False
 
-        ignored = []
+    def parser(self, args: Optional[str | list] = None, rules: Optional[dict] = None,
+               keys: Optional[bool] = False, wasv: Optional[bool] = False,
+               ignored: Optional[bool] = False) -> dict | tuple[dict, tuple]:
+        """ args:    Arguments to parse.
+            rules:   Rules for parsing arguments.
+            keys:    Output with key names. Example: {'Key': ('argument', 'value')}.
+            wasv:    Output with all single values (True and False).
+            ignored: Get two output values: Parsed arguments & Ignored arguments. """
+
+        self._set_args(args, rules, wasv, keys, ignored)
+
+        args = self.args[:]
+        self.keys_used = []
+        ignored_values = []
         output = {}
 
-        pairs = rules.get('pairs')
-        single = rules.get('single')
-        united = rules.get('united')
+        pairs: dict = self.rules.get('pairs')
+        single: dict = self.rules.get('single')
 
-        assert pairs or single or united, self.help
+        assert pairs or single, self.help
 
         if isinstance(args, tuple):
             args = list(args)
         elif isinstance(args, list):
-            # while '' in args:
-            # args.remove('')
             tmp = []
             for arg in args:
                 if ' ' in arg and not ('="' in arg or "='" in arg) and \
@@ -346,66 +350,31 @@ class ArgParser:
 
         args = self.strings_parser(args)
 
-        assert isinstance(args, list), f'args = {args} is not valid.'
+        assert isinstance(args, list), f"args = {args} is not valid."
 
         args = self.pairs_union(args)
 
-        self.keys_used = []
-
         while args:
             arg = args.pop(0)
-            ignore = True
 
-            if pairs and args:  # Validate Pairs: -Arg Value
-                ignore = self.pairs_vals(arg, args, pairs, output, wn)
+            if pairs:  # Validate Pairs (key value): -Arg Value
+                ignore = self.pairs_vals(arg, args, pairs, output)
+                if not ignore:
+                    continue
 
-            if not ignore:
-                continue
+            if single:  # Validate Single (bool): -Arg
+                ignore = self.single_vals(arg, single, output)
+                if not ignore:
+                    continue
 
-            if single:  # Validate Single: -Arg
-                ignore = self.single_vals(arg, single, output, wn)
+            ignored_values.append(arg)
 
-            if not ignore:
-                continue
+        if wasv and single:
+            self._set_wasv(single, output)
 
-            if united:  # Validate United: -Arg = Value, -Arg: Value
-                ignore = self.united_vals(
-                    arg, united, output, ignored, wn)
+        if self.ignored:
+            # Output Values with Arguments and Ignored Values.
+            return output, tuple(ignored_values)
 
-            if ignore == 'continue' or not ignore:
-                continue
-
-            ignored.append(arg)
-
-        if single and wn and wasv:
-            arguments = [argument for argument, content in output.values()]
-            for key, value in single.items():
-                if value.__class__ in (list, tuple):
-                    in_output = [False, value[0]]
-                    for val in value:
-                        if val in arguments:
-                            in_output = [True, val]
-                            break
-                    if not in_output[0]:
-                        output[key] = (in_output[1], False)
-                elif not key in arguments:
-                    output[key] = (value, False)
-        elif single and not wn and wasv:
-            for value in single.values():
-                if value.__class__ in (list, tuple):
-                    in_output = [False, value[0]]
-                    for val in value:
-                        if val in output:
-                            in_output = [True, val]
-                            break
-                    if not in_output[0]:
-                        output[in_output[1]] = False
-                elif not value in output:
-                    output[value] = False
-
-        # Reset params:
-        self.wasv = False
-        self.wn = False
-
-        # Output Values with Arguments and Values Ignored.
-        return output, tuple(ignored)
+        # Output Argument Values
+        return output
